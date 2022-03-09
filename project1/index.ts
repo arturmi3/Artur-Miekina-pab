fc1e79cc3410db45e154f454d697c479797e398f
Artur Miękina <60198804+arturmi3@users.noreply.github.com>
2022-03-09 23:14:21 +0100

    update12

--- project1/index.ts
+++ project1/index.ts
@@ -1,4 +1,27 @@
 const express = require('express') 
 const app = express() 
-app.get('/', function (req, res) { res.send('Hello World') }) 
+app.get('/', function (req, res) 
+{ 
+    const oper = req.query.oper;
+  const l1 = req.query.l1;
+  const l2 = req.query.l2;
+
+  switch (oper) {
+    case 'add':
+        res.send({'result = ': (l1 + l2)});
+        break;
+    case 'multiply':
+        res.send({'result = ': (l1 * l2)});
+        break;
+    case 'divide':
+        res.send({'result = ': (l1 / l2)});
+        break;
+    case 'minus':
+        res.send({'result = ': (l1 - l2)});
+        break;
+    default:
+        res.send('Hello World')
+  } 
+}
+) 
 app.listen(3000)
